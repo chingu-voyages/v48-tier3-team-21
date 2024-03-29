9418fc9002d00cbc84ba4684112e39d1f9d3451d
Jaweki <werukioni@gmail.com>
2024-03-29 10:32:02 +0300

    created pages for about-us, privacy-policy and terms-and-conditions

--- dinosaur_app/app/lib/constants.ts
+++ dinosaur_app/app/lib/constants.ts
@@ -1,33 +1,49 @@
+
 export const developers = [
   {
     name: "Jack Weru",
     avatar: "https://avatars.githubusercontent.com/u/97351534?v=4",
     github: "https://github.com/Jaweki",
+    linked_in: "https://linkedin.com/in/jaweki-dekut",
+    website: "https://portfolio.jaweki.com",
     role: "web developer",
+    description: "Telecommunication and information Engineer. Web-frontends and backends, Network design and administration, embedded systems design, CAD (Autodesk Fusion 360). See my website for more awesome projects...",
   },
   {
     name: "Shivam",
-    avatar: "",
-    github: "",
+    avatar: "https://avatars.githubusercontent.com/u/91086992?v=4",
+    github: "https://github.com/shivam24161",
+    linked_in: "https://linkedin.com/in/shivam",
+    website: "https://github.com/shivam24161",
     role: "web developer",
+    description: "Shivam has not added a description.",
   },
   {
     name: "Tanisha",
     avatar: "",
     github: "",
+    linked_in: "https://linkedin.com/in/tanisha",
+    website: "https://",
     role: "UI/UX designer",
+    description: "Tanisha has not added a description",
   },
   {
     name: "Arman Jena",
-    avatar: "",
-    github: "",
+    avatar: "https://avatars.githubusercontent.com/u/112967686?v=4",
+    github: "https://github.com/Ajen07",
+    linked_in: "https://linkedin.com/in/arman",
+    website: "https://github.com/Ajen07",
     role: "web developer",
+    description: "Full Stack developer | DevOps and Cloud enthusiast"
   },
   {
-    name: "Shant Dashian",
-    avatar: "",
-    github: "",
-    role: "scrum master",
+    name: "Shant Dashjian",
+    avatar: "https://avatars.githubusercontent.com/u/2113918?v=4",
+    github: "https://github.com/shantdashjian",
+    linked_in: "https://www.linkedin.com/in/shantdashjian",
+    website: "https://shantdashjian.com/",
+    role: "web developer",
+    description: "I bring 10 years of diverse experience in the software industry. I'm an effective problem solver, engineer, and mentor delivering solutions building full-stack web and microservices architecture based applications. I enjoy mentoring developers and teams on Test Driven Development (TDD) and Pair Programming. ",
   },
 ];
 
